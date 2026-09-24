64550429005c83d30ef9eed80ac238db4b741d44
Felipe-Pqd90599 <Felipe-Pqd90599@users.noreply.github.com>
2026-09-24 01:12:01 +0000

    deploy: 52abb9c019394825f187216eb2e537d16bb0af93

--- app.js
+++ app.js
@@ -56,16 +56,26 @@ function renderCategoryGrids(cat, menu, itemRenderer) {
     const byId = Object.fromEntries(allItems.map((item) => [item.id, item]));
     return cat.groups
       .map((group) => {
-        const cards = (group.itemIds || [])
+        const ids = group.itemIds || [];
+        const featuredSet = new Set(group.featuredIds || []);
+        const featured = (group.featuredIds || []).map((id) => byId[id]).filter(Boolean);
+        const compact = ids
+          .filter((id) => !featuredSet.has(id))
           .map((id) => byId[id])
-          .filter(Boolean)
-          .map(renderOne)
-          .join('');
-        if (!cards) return '';
+          .filter(Boolean);
+        if (!featured.length && !compact.length) return '';
+
+        let grids = '';
+        if (featured.length) {
+          grids += `<div class="section__grid">${featured.map(renderOne).join('')}</div>`;
+        }
+        if (compact.length) {
+          grids += `<div class="section__grid section__group-compact">${compact.map(renderOne).join('')}</div>`;
+        }
         return `
           <div class="section__group">
             <h3 class="section__subtitle">${group.name}</h3>
-            <div class="section__grid">${cards}</div>
+            ${grids}
           </div>`;
       })
       .join('');
